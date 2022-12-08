8c2fb209680baeb4025feef78d721adf50a25d2b
Ayumi Takahashi <ayumee528@gmail.com>
2022-12-08 12:56:21 -0800

    Added deploy settings for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/astar-network'
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
   image: {
     domains: ['img.youtube.com'],
