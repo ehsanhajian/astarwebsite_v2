5b3daed22e0a2d06a184a66b0b746010351a26d4
Sota20 <121214678+Sota20@users.noreply.github.com>
2023-02-07 10:18:40 +0900

    Update mentors.ts

--- content/mentors.ts
+++ content/mentors.ts
@@ -10,8 +10,8 @@ export const mentors = [
       ja: "Astar Network ファウンダー, Startale Labs 最高経営責任者",
     },
     profile: {
-      en: "Sota is the Founder and CEO of Astar Network. Prior to creating the Smart Contract HUB for WASM + EVM and one of the top Parachains on Polkadot, he was the Director at the Japan Blockchain Association. He is selected Forbes 30 Under 30 Asia in 2022. He was also a task force member of the Trusted Web Council led by the Cabinet Secretariat, Government of Japan. Prior to that, he worked at Chronicled in San Francisco and was a blockchain researcher at the University of Tokyo.",
-      ja: "Astar Networkの創設者兼CEO。WASM + EVMのスマートコントラクトHUBやPolkadotでトップクラスのパラチェンを作る前は、日本ブロックチェーン協会で理事。2022年のForbes 30 Under 30 Asiaに選出される。内閣官房が主導する「Trusted Web 推進協議会」のタスクフォースメンバー。それ以前はサンフランシスコのChronicled社に勤務し、東京大学でブロックチェーン研究員を務める。",
+      en: "After an internship in Silicon Valley, and as graduate blockchain co-researcher at the University of Tokyo, I founded Stake Technologies in 2019, and became the founder of Astar Network. I’m also a board member of Japan Blockchain Association (JBA) and an advisor/advisor to GMO Web3, Dentsu Web3, and Marui Group.In 2022, the company was named Forbes Asia 30 Under 30 and Forbes Japan 30 Under 30 in the technology category. In 2023, he founded Startale Labs, a web3 development company, and will collaborate with large Japanese companies on projects to create use cases utilizing the Astar Network.",
+      ja: "シリコンバレーでのインターンシップ、東京大学大学院ブロックチェーン共同研究員を経て、2019年Stake Technologiesを創業。日本発のパブリックブロックチェーンであるAstar Networkの創業者となる。Next Web Capitalファウンダー、日本ブロックチェーン協会（JBA）理事や、GMO Web3、電通Web3、や丸井グループのアドバイザー/顧問も務める。2022年、Forbes紙の選ぶテクノロジー部門 Forbes Asia 30 Under 30に選出。同年、Forbes Japan 30 Under 30にも選出された。2023年にはweb3開発会社であるStartale Labsを創業し、Astar Networkを活用したユースケースを作るために日本の大企業とコラボレーションして案件を進める。",
     },
     image: "sota.jpg",
     expertise: [
