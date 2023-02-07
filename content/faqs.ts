1bfa44b8b1b2b5f201a3ce03da086ed044a9d87d
Sota20 <121214678+Sota20@users.noreply.github.com>
2023-02-08 01:11:15 +0900

    Update faqs.ts

--- content/faqs.ts
+++ content/faqs.ts
@@ -1,7 +1,7 @@
 export const faqs = [
   {
     question: {
-      en: "Question 1",
+      en: "What kind of companies can participate in the incubation program?",
       ja: "質問 1",
     },
     answer: {
@@ -11,7 +11,83 @@ export const faqs = [
   },
   {
     question: {
-      en: "Question 2",
+      en: "What is the content of the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },
+  {
+    question: {
+      en: "What can be gained by participating in the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },{
+    question: {
+      en: "How is the incubation program carried out?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "",
+      ja: "回答 2",
+    },
+  },{
+    question: {
+      en: "What support is provided to companies that participate in the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },{
+    question: {
+      en: "What are the requirements for participating in the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },{
+    question: {
+      en: "What expectations can companies that participate in the incubation program have?
+",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },{
+    question: {
+      en: "How can a relationship with Sony Network Communications be built by participating in the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },
+  {
+    question: {
+      en: "How can knowledge and technology related to Web3 be improved by participating in the incubation program?",
+      ja: "質問 2",
+    },
+    answer: {
+      en: "Answer 2",
+      ja: "回答 2",
+    },
+  },
+  {
+    question: {
+      en: "What is the application process for participating in the incubation program?",
       ja: "質問 2",
     },
     answer: {
