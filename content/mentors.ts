de6cef67b3e4c0b88f1dc4794c7b38ba30d7954f
Sota Watanabe <w.souta.s.life@gmail.com>
2023-02-14 12:38:04 +0800

    Update mentors.ts

--- content/mentors.ts
+++ content/mentors.ts
@@ -3,15 +3,15 @@ export const mentors = [
     index: true,
     name: {
       en: "Ryohei Suzuki",
-      ja: "鈴木 良平",
+      ja: "Ryohei Suzuki",
     },
     title: {
       en: "Director at Sony Network Communications Singapore",
-      ja: "Sony Network Communications Singapore ディレクター",
+      ja: "Director at Sony Network Communications Singapore",
     },
     profile: {
       en: "Ryohei Suzuki, Director, Sony Network Communications Singapore Ryohei Suzuki has been involved in the launch of multiple new businesses within the Sony Group, and currently oversees the Web3/NFT business in the Sony Network Communications. He has been appointed Director of Sony Network Communications Singapore, which promotes the Web3/NFT business established in April 2022.",
-      ja: "これまでソニーグループ内で複数の新規事業の立ち上げに携わる。現在、Sony Network Communications事業開発部でWeb3/NFT事業を管轄。2022年4月に設立したWeb3/NFT事業を推進するSony Network Communications SingaporeのDirectorに就任。",
+      ja: "Ryohei Suzuki, Director, Sony Network Communications Singapore Ryohei Suzuki has been involved in the launch of multiple new businesses within the Sony Group, and currently oversees the Web3/NFT business in the Sony Network Communications. He has been appointed Director of Sony Network Communications Singapore, which promotes the Web3/NFT business established in April 2022.",
     },
     image: "suzuki.jpg",
     expertise: [],
@@ -25,15 +25,15 @@ export const mentors = [
     index: true,
     name: {
       en: "Teemu Pohjola",
-      ja: "テーム・ポホヨラ",
+      ja: "Teemu Pohjola",
     },
     title: {
       en: "General manager at Sony R&D Center Brussels lab (Deputy Head)",
-      ja: "Sony R&D Center Brussels lab ゼネラルマネージャー（副所長）",
+      ja: "General manager at Sony R&D Center Brussels lab (Deputy Head)",
     },
     profile: {
       en: "Within Sony Teemu has been turning brilliant engineering into viable business and delightful user experiences.",
-      ja: "ソニーでは、優れたエンジニアリングをビジネスとして成立させ、快適なユーザー体験を提供してきました。",
+      ja: "Within Sony Teemu has been turning brilliant engineering into viable business and delightful user experiences.",
     },
     image: "teemu.jpg",
     expertise: [],
@@ -47,20 +47,20 @@ export const mentors = [
     index: true,
     name: {
       en: "Sota Watanabe",
-      ja: "渡辺創太",
+      ja: "Sota Watanabe",
     },
     title: {
       en: "Founder of Astar Network, CEO at Startale Labs",
-      ja: "Astar Network ファウンダー, Startale Labs 最高経営責任者(CEO)",
+      ja: "Founder of Astar Network, CEO at Startale Labs",
     },
     profile: {
       en: "After an internship in Silicon Valley, and as graduate blockchain co-researcher at the University of Tokyo, I founded Stake Technologies in 2019, and became the founder of Astar Network. I’m also a board member of Japan Blockchain Association (JBA) and an advisor/advisor to GMO Web3, Dentsu Web3, and Marui Group.In 2022, the company was named Forbes Asia 30 Under 30 and Forbes Japan 30 Under 30 in the technology category. In 2023, he founded Startale Labs, a web3 development company, and will collaborate with large Japanese companies on projects to create use cases utilizing the Astar Network.",
-      ja: "シリコンバレーでのインターンシップ、東京大学大学院ブロックチェーン共同研究員を経て、2019年Stake Technologiesを創業。日本発のパブリックブロックチェーンであるAstar Networkの創業者となる。Next Web Capitalファウンダー、日本ブロックチェーン協会（JBA）理事や、GMO Web3、電通Web3、や丸井グループのアドバイザー/顧問も務める。2022年、Forbes紙の選ぶテクノロジー部門 Forbes Asia 30 Under 30に選出。同年、Forbes Japan 30 Under 30にも選出された。2023年にはweb3開発会社であるStartale Labsを創業し、Astar Networkを活用したユースケースを作るために日本の大企業とコラボレーションして案件を進める。",
+      ja: "After an internship in Silicon Valley, and as graduate blockchain co-researcher at the University of Tokyo, I founded Stake Technologies in 2019, and became the founder of Astar Network. I’m also a board member of Japan Blockchain Association (JBA) and an advisor/advisor to GMO Web3, Dentsu Web3, and Marui Group.In 2022, the company was named Forbes Asia 30 Under 30 and Forbes Japan 30 Under 30 in the technology category. In 2023, he founded Startale Labs, a web3 development company, and will collaborate with large Japanese companies on projects to create use cases utilizing the Astar Network.",
     },
     image: "sota.jpg",
     expertise: [
-      { en: "Infrastructure", ja: "インフラ" },
-      { en: "Product", ja: "プロダクト開発" },
+      { en: "Infrastructure", ja: "Infrastructure" },
+      { en: "Product", ja: "Product" },
     ],
     links: {
       website: "https://astar.network/",
@@ -72,21 +72,21 @@ export const mentors = [
     index: false,
     name: {
       en: "Maarten Henskens",
-      ja: "マーティン・ヘンスケンス",
+      ja: "Maarten Henskens",
     },
     title: {
       en: "Chief Growth Officer at Astar Network",
-      ja: "Astar Network 最高事業成長責任者(CGO)",
+      ja: "Chief Growth Officer at Astar Network",
     },
     profile: {
       en: "Maarten Henskens is the Chief Growth Officer at Astar Network. As a modern-day renaissance man building on the foundations of the open web, his first interaction with blockchain was in 2013 when he bought his first Bitcoin. From that moment, he was hooked on blockchain technology and the culture behind it.",
-      ja: "マーティン・ヘンスケンスはAstar Networkの最高事業成長責任者(CGO)です。オープンウェブの基礎の上に構築された現代のルネッサンスマンとして、彼が初めてブロックチェーンに触れたのは、2013年に初めてビットコインを購入した時でした。その瞬間からブロックチェーン技術とその背後にある文化の虜になりました。",
+      ja: "Maarten Henskens is the Chief Growth Officer at Astar Network. As a modern-day renaissance man building on the foundations of the open web, his first interaction with blockchain was in 2013 when he bought his first Bitcoin. From that moment, he was hooked on blockchain technology and the culture behind it.",
     },
     image: "maarten.jpg",
     expertise: [
-      { en: "Biz Dev", ja: "事業開発" },
-      { en: "Growth", ja: "グロース" },
-      { en: "Product", ja: "プロダクト開発" },
+      { en: "Biz Dev", ja: "Biz Dev" },
+      { en: "Growth", ja: "Growth" },
+      { en: "Product", ja: "Product" },
     ],
     links: {
       website: "https://astar.network/",
@@ -98,23 +98,23 @@ export const mentors = [
     index: false,
     name: {
       en: "Andrew Vranjes",
-      ja: "アンドリュー・ヴランジェス",
+      ja: "Andrew Vranjes",
     },
     title: {
       en: "VP/GM APAC at Blockdaemon",
-      ja: "Blockdaemon APAC バイスプレジデント/ゼネラルマネージャー",
+      ja: "VP/GM APAC at Blockdaemon",
     },
     profile: {
       en: "Andrew is leading Blockdaemon’s efforts to build out APAC, as VP Sales and GM. Previously Andrew has had leadership roles in Amazon Web Services, Cisco Systems and Singtel/Optus. Andrew has been based in Singapore for over a decade and brings strong networks in startups, digital natives, crypto native, venture capital and private equity networks. Andrew is focused on supporting customers, growing the Blockdaemon business, hiring and developing the Blockdaemon culture in APAC. \n\nIn addition, Andrew has been investing in web2 and web3 companies for over a decade. Andrew has made angel investments into well-known Startups in the region such as 99.co, MoneySmart, CodaPay, Shopup, Shopback and Homage to mention a few. Andrew is a Kauffman Fellow, Class 23. Andrew has invested in ~20 web3 startups - such as crabada, minterest, portal, swan exchange and SSV to name a few.  In Japan, Andrew was a seed round investor in makeleaps.jp - which was funded by Rakuten and later acquired by Ricoh.",
-      ja: "アンドリューはセールスのバイスプレジデント兼ゼネラルマネージャーとして、BlockdaemonのAPAC構築の取り組みをリードしています。これまでAmazon Web Services、Cisco Systems、Singtel/Optusにおいて、リーダーとしての役割を担ってきました。シンガポールに10年以上拠点を置き、スタートアップ企業、デジタルネイティブ、暗号ネイティブ、ベンチャーキャピタル、プライベートエクイティのネットワークにおいて強力なネットワークを構築しています。アンドリューは、顧客のサポート、Blockdaemonのビジネスの成長、APACにおけるBlockdaemonの雇用と文化の発展に注力しています。\n\nまた、10年以上にわたってWeb2およびWeb3企業への投資を行ってきました。99.co, MoneySmart, CodaPay, Shopup, Shopback, Homageなど、この地域で有名なスタートアップにエンジェル投資を行っています。これまでに、crabada、minterest、portal、swan exchange、SSVなど、20社以上のWeb3スタートアップに投資しています。日本では楽天が出資し後にリコーが買収したmakeleaps.jpのシードラウンドの投資家として活躍しました。",
+      ja: "Andrew is leading Blockdaemon’s efforts to build out APAC, as VP Sales and GM. Previously Andrew has had leadership roles in Amazon Web Services, Cisco Systems and Singtel/Optus. Andrew has been based in Singapore for over a decade and brings strong networks in startups, digital natives, crypto native, venture capital and private equity networks. Andrew is focused on supporting customers, growing the Blockdaemon business, hiring and developing the Blockdaemon culture in APAC. \n\nIn addition, Andrew has been investing in web2 and web3 companies for over a decade. Andrew has made angel investments into well-known Startups in the region such as 99.co, MoneySmart, CodaPay, Shopup, Shopback and Homage to mention a few. Andrew is a Kauffman Fellow, Class 23. Andrew has invested in ~20 web3 startups - such as crabada, minterest, portal, swan exchange and SSV to name a few.  In Japan, Andrew was a seed round investor in makeleaps.jp - which was funded by Rakuten and later acquired by Ricoh.",
     },
     image: "andrew.jpg",
     expertise: [
-      { en: "Scaling companies", ja: "企業の規模拡大" },
-      { en: "Sales", ja: "セールス" },
-      { en: "Biz Dev", ja: "事業開発" },
-      { en: "Hiring", ja: "採用" },
-      { en: "GTM Strategy", ja: "市場進出戦略" },
+      { en: "Scaling companies", ja: "Scaling companies" },
+      { en: "Sales", ja: "Sales" },
+      { en: "Biz Dev", ja: "Biz Dev" },
+      { en: "Hiring", ja: "Hiring" },
+      { en: "GTM Strategy", ja: "GTM Strategy" },
     ],
     links: {
       website: "https://blockdaemon.com/",
@@ -126,21 +126,21 @@ export const mentors = [
     index: false,
     name: {
       en: "Glenn Woo",
-      ja: "グレン・ウー",
+      ja: "Glenn Woo",
     },
     title: {
       en: "Head of Sales APAC at Blockdaemon",
-      ja: "Blockdaemon APAC セールス ヘッド",
+      ja: "Head of Sales APAC at Blockdaemon",
     },
     profile: {
       en: "Glenn joined Blockdaemon in April 2022 as Head of Sales APAC. Blockdaemon offers institutional grade blockchain infrastructures connecting enterprises to blockchains. He joined the blockchain industry in 2018 as Managing Director, Head of APAC for Ledger. Glenn has an extensive career in the financial services and fintech, working for S&P Global Market intelligence as Head of North Asia based in Korea and Hong Kong for 9 years, and prior to that, at Shinhan Financial Group in asset custody and funds administration.",
-      ja: "グレンは2022年4月にAPACセールスのヘッドとしてBlockdaemonに入社しました。Blockdaemonは企業とブロックチェーンをつなぐインフラストラクチャーを提供しています。2018年にLedgerのAPAC責任者であるマネージングディレクターとしてブロックチェーン業界に参入しました。グレンは金融サービスとフィンテックの分野で幅広いキャリアを持ち、S&P Global Market intelligenceで9年間、韓国と香港を拠点とする北アジアのヘッドとして働き、それ以前は新韓金融グループで資産保管と資金管理業務に携わっています。",
+      ja: "Glenn joined Blockdaemon in April 2022 as Head of Sales APAC. Blockdaemon offers institutional grade blockchain infrastructures connecting enterprises to blockchains. He joined the blockchain industry in 2018 as Managing Director, Head of APAC for Ledger. Glenn has an extensive career in the financial services and fintech, working for S&P Global Market intelligence as Head of North Asia based in Korea and Hong Kong for 9 years, and prior to that, at Shinhan Financial Group in asset custody and funds administration.",
     },
     image: "glenn.jpg",
     expertise: [
-      { en: "Infrastructure (Node, Staking, Custody)", ja: "インフラ (Node, Staking, Custody)" },
-      { en: "Finance", ja: "ファイナンス" },
-      { en: "Data & Analytics", ja: "データ&分析" },
+      { en: "Infrastructure (Node, Staking, Custody)", ja: "Infrastructure (Node, Staking, Custody)" },
+      { en: "Finance", ja: "Finance" },
+      { en: "Data & Analytics", ja: "Data & Analytics" },
     ],
     links: {
       website: "https://blockdaemon.com/",
@@ -152,22 +152,22 @@ export const mentors = [
     index: false,
     name: {
       en: "Wei Shi Khai",
-      ja: "ウェイ・シー・カイ",
+      ja: "Wei Shi Khai",
     },
     title: {
       en: "General Partner & COO at LongHash Ventures",
-      ja: "LongHash Ventures ジェネラルパートナー, 最高執行責任者(COO)",
+      ja: "General Partner & COO at LongHash Ventures",
     },
     profile: {
       en: "Shi Khai leads LongHash Ventures, which specializes in bootstrapping Web3 ecosystems. LongHashX Accelerator is Asia’s first and leading Web3 accelerator, which supported >60 web3 startups and helped them raise >$150m in funding. LongHashX partners include Polkadot, Filecoin, and Axelar, and notable alumni include Astar, Xanpool, and Lit Protocol.",
-      ja: "シー・カイは、Web3エコシステムのブートストラップを専門とするLongHash Venturesを率いています。LongHashX Acceleratorは、アジア初の主要なWeb3アクセラレータであり、60以上のWeb3スタートアップを支援し、1億5000万ドル以上の資金調達に貢献しています。LongHashXのパートナーには、Polkadot、Filecoin、Axelarなどがあり、著名な卒業生にはAstar、Xanpool、Lit Protocolなどがいます。",
+      ja: "Shi Khai leads LongHash Ventures, which specializes in bootstrapping Web3 ecosystems. LongHashX Accelerator is Asia’s first and leading Web3 accelerator, which supported >60 web3 startups and helped them raise >$150m in funding. LongHashX partners include Polkadot, Filecoin, and Axelar, and notable alumni include Astar, Xanpool, and Lit Protocol.",
     },
     image: "wei.jpg",
     expertise: [
-      { en: "Infrastructure (Multi-chain)", ja: "インフラ (Multi-chain)" },
+      { en: "Infrastructure (Multi-chain)", ja: "Infrastructure (Multi-chain)" },
       { en: "DAOs", ja: "DAO" },
-      { en: "Tokenomics", ja: "トークノミクス" },
-      { en: "Strategy", ja: "戦略" },
+      { en: "Tokenomics", ja: "Tokenomics" },
+      { en: "Strategy", ja: "Strategy" },
     ],
     links: {
       website: "https://www.longhash.vc",
@@ -179,19 +179,19 @@ export const mentors = [
     index: false,
     name: {
       en: "Yuki Yuminaga",
-      ja: "ユウキ・ユミナガ",
+      ja: "Yuki Yuminaga",
     },
     title: {
       en: "Investor & Researcher at Fenbushi Capital",
-      ja: "Fenbushi Capital 投資家, リサーチャー",
+      ja: "Investor & Researcher at Fenbushi Capital",
     },
     profile: {
       en: 'I am an investor and researcher at Fenbushi Capital. Always love building and researching alongside the innovators in the space! Love to brainstorm about ways to break a system ("trustless" something something) and dream one day of economically attacking one also ;)',
-      ja: "Fenbushi Capitalで投資家兼リサーチャーをしています。この分野のイノベーターと一緒に何かを作り、研究することが大好きです。システムを破壊する方法についてブレインストーミングするのが好きで、いつか経済的に攻撃することを夢見ています。",
+      ja: "I am an investor and researcher at Fenbushi Capital. Always love building and researching alongside the innovators in the space! Love to brainstorm about ways to break a system ("trustless" something something) and dream one day of economically attacking one also ;)",
     },
     image: "yuki.jpg",
     expertise: [
-      { en: "Infrastructure (Consensus/MEV/privacy)", ja: "インフラ (Consensus/MEV/privacy)" },
+      { en: "Infrastructure (Consensus/MEV/privacy)", ja: "Infrastructure (Consensus/MEV/privacy)" },
       { en: "DeFi", ja: "DeFi" },
     ],
     links: {
