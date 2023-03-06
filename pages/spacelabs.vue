596e312ea6c5a0de014e2f59dcee243501a701dc
impelcrypto <impelcrypto@gmail.com>
2023-03-06 17:09:02 +0800

    feat: redirect from '/spacelabs' to '/community'

---
+++ pages/spacelabs.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { watchEffect } from 'vue';
+import { useRouter } from 'vue-router';
+
+definePageMeta({
+  layout: false,
+  title: 'Spacelabs',
+  slug: 'spacelabs',
+  description: 'A star is born together with our awesome community.',
+});
+const router = useRouter();
+watchEffect(() => {
+  router.push('/community');
+});
+</script>
+
+<style lang="postcss" scoped>
+.dummy {
+  margin: auto;
+}
+</style>
