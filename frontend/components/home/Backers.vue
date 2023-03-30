d6ae2c3c569bb500b2737100100f27693a591aa3
Ayumi Takahashi <ayumee528@gmail.com>
2023-03-30 13:20:58 -0700

    Fetch backer logos from strapi db on the homepage

--- frontend/components/home/Backers.vue
+++ frontend/components/home/Backers.vue
@@ -8,11 +8,11 @@
     <div
       class="grid grid-cols-2 gap-x-6 sm:gap-x-8 gap-y-12 sm:gap-y-20 sm:grid-cols-4 lg:grid-cols-5"
     >
-      <div v-for="item in logos" class="flex items-center justify-center">
+      <div v-for="item in projects" class="flex items-center justify-center">
         <img
+          :src="useStrapiMedia(item.attributes.logo.data.attributes.url)"
+          :alt="item.attributes.name"
           class="h-12 w-32 lg:w-48 object-contain"
-          :src="'/images/home/backers/' + item.image"
-          :alt="item.name"
         />
       </div>
     </div>
@@ -20,114 +20,39 @@
 </template>
 
 <script setup lang="ts">
-const logos = [
-  {
-    name: "Binance Labs",
-    image: "binance-labs.png",
-  },
-  {
-    name: "Coinbase Ventures",
-    image: "coinbase-ventures.png",
-  },
-  {
-    name: "Polychain Capital",
-    image: "polychain-capital.png",
-  },
-  {
-    name: "Crypto.com Capital",
-    image: "cryptocom-capital.png",
-  },
-  {
-    name: "Fenbushi Capital",
-    image: "fenbushi-capital.png",
-  },
-  {
-    name: "Hypersphere",
-    image: "hypersphere.png",
-  },
-  {
-    name: "Hashkey Capital",
-    image: "hashkey-capital.svg",
-  },
-  {
-    name: "Huobi Ventures",
-    image: "huobi-ventures.png",
-  },
-  {
-    name: "Alchemy Ventures",
-    image: "alchemy-ventures.svg",
-  },
-  {
-    name: "DFG",
-    image: "dfg.png",
-  },
-  {
-    name: "KR1",
-    image: "kr1.png",
-  },
-  {
-    name: "Okex Blockdream Ventures",
-    image: "okex-blockdream-ventures.png",
-  },
-  {
-    name: "Longhash Ventures",
-    image: "longhash-ventures.png",
-  },
-  {
-    name: "gumi Cryptos",
-    image: "gumi-cryptos.png",
-  },
-  {
-    name: "Sub0 capital",
-    image: "sub0capital.png",
-  },
-  {
-    name: "Digital Strategies",
-    image: "digital-strategies.png",
-  },
-  {
-    name: "Altonomy",
-    image: "altonomy.svg",
-  },
-  {
-    name: "AU21 Capital",
-    image: "au21capital.png",
-  },
-  {
-    name: "TRG Capital",
-    image: "trg-capital.png",
-  },
-  {
-    name: "PAKA",
-    image: "paka.png",
-  },
-  {
-    name: "SNZ",
-    image: "snz.png",
-  },
-  {
-    name: "IOSG Ventres",
-    image: "iosg-ventres.png",
-  },
-  {
-    name: "GSR",
-    image: "gsr.svg",
-  },
-  {
-    name: "Rok Capital",
-    image: "rok-capital.png",
-  },
-  {
-    name: "Scytale Ventures",
-    image: "scytale-ventures.png",
-  },
-  {
-    name: "Vessel",
-    image: "vessel.png",
-  },
-  {
-    name: "Warburg Serres",
-    image: "warburg-serres.png",
-  },
-];
+import gql from "graphql-tag";
+
+const query = gql`
+  query getAllData {
+    projects(
+      pagination: { page: 1, pageSize: 100 }
+      filters: { project_categories: { id: { eq: 9 } } }
+    ) {
+      data {
+        attributes {
+          name
+          website
+          description
+          logo {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+const { data } = await useAsyncQuery(query);
+
+let projects = [];
+projects = data.value.projects.data.sort((a, b) => {
+  if (a.attributes.name.toLowerCase() > b.attributes.name.toLowerCase()) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
 </script>
