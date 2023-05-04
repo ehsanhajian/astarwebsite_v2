db36b6235aca6586385713d244967cb652517d14
Ayumi Takahashi <ayumee528@gmail.com>
2023-05-03 22:46:49 -0700

    Add filterd by tag page

---
+++ pages/blog/tag.vue
@@ -0,0 +1,108 @@
+<template>
+  <NuxtLayout name="default">
+    <template #space>
+      <div class="space-gradient relative">
+        <img
+          class="fixed z-[3] portrait:h-screen landscape:w-screen object-cover"
+          src="/images/common/space-stars.svg"
+          alt=""
+          width="1728"
+          height="1728"
+        />
+        <div
+          class="min-h-[50vh] sm:min-h-[60vh] flex items-center justify-start"
+        >
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 relative z-10 w-full">
+            <div class="text-center">
+              <p class="text-2xl">Tag:</p>
+              <h1
+                class="text-4xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight drop-shadow"
+              >
+                TagName here
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 relative z-10">
+        <ul
+          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12"
+        >
+          <BlogArticleCard v-for="post in posts" :post="post" />
+        </ul>
+      </div>
+    </template>
+    <template #earth>
+      <Footer />
+    </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import gql from "graphql-tag";
+
+// The subsocial space where the dApp staking news updates come from: https://polkaverse.com/11132
+const astarSpace = 11132;
+const query = gql`
+  query PostsBySpaceId {
+    posts(where: { space: { id_eq: "${astarSpace}" } }, orderBy: id_DESC) {
+      publishedDate: createdOnDay
+      title
+      href: canonical
+      image
+      id
+    }
+  }
+`;
+
+const { data } = await useAsyncQuery({ query, clientId: "subsocial" });
+const posts = data.value.posts.map(
+  (item: { publishedDate: string | number | Date }) => {
+    const date = new Date(item.publishedDate);
+    const formattedDate = date.toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+    return {
+      ...item,
+      image: item.image
+        ? "https://ipfs.subsocial.network/ipfs/" + item.image
+        : "/images/blog/placeholder.webp",
+      publishedDate: formattedDate,
+    };
+  }
+);
+
+import { meta } from "../../content/meta";
+const seoTitle = `Tag | ${meta.siteName} - ${meta.tagline}`;
+const seoDescription = "Tag description";
+
+useServerSeoMeta({
+  ogTitle: () => seoTitle,
+  title: () => seoTitle,
+  description: () => seoDescription,
+  ogDescription: () => seoDescription,
+  ogImage: () => meta.image,
+  ogImageUrl: () => meta.image,
+  twitterCard: () => "summary_large_image",
+  twitterTitle: () => seoTitle,
+  twitterDescription: () => seoDescription,
+  twitterImage: () => meta.image,
+});
+
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<style lang="postcss" scoped>
+.space-gradient::before {
+  @apply w-full h-full absolute content-[""] z-[0];
+  background: linear-gradient(
+    180deg,
+    rgba(8, 16, 41, 0) 30%,
+    rgba(8, 16, 41, 100) 100%
+  );
+}
+</style>
